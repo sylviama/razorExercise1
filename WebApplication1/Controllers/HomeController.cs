df5206704033b60cb8426f6519893f26e884a756
Sylvia Ma <xiangwma@umich.edu>
2016-09-14 12:11:19 -0500

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Names = new string[]
+            {
+                "Sylvia",
+                "Mark",
+                "Matt",
+                "Alaric",
+                "Eric"
+            };
+
+            ViewBag.Name = "This is Sylvia's Page";
+            ViewBag.Greeting = "Welcome! Warm Greeting!";
+            DateTime today = DateTime.Today;
+            if(today.DayOfWeek==DayOfWeek.Friday)
+            {
+                ViewBag.IfIsFri = "TGIF";
+            }
+            else
+            {
+                ViewBag.IfIsFri = "Nope";
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
